e4304589e7763681256992bc7cc949e094941c6c
Yuri Burk <yuriburk@gmail.com>
2021-12-03 17:57:05 -0300

    Next PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>Burk.dev Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#05092B" />
         <meta name="description" content="A boilerplate with Next JS" />
       </Head>
       <GlobalStyle />
