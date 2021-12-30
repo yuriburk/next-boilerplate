48ce95f045bedcb7a19d310272b3d01b01fdcb84
Yuri Burk <yuriburk@gmail.com>
2021-12-30 14:37:37 -0300

    Jest

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
 }
