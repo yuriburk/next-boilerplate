ffdfc3e05dff6e24c7838a9b93b0d351969e461a
Yuri Burk <yuriburk@gmail.com>
2022-01-04 12:17:10 -0300

    Redux

--- src/components/ComponentWrapper/index.tsx
+++ src/components/ComponentWrapper/index.tsx
@@ -6,7 +6,6 @@ import { useAppSelector } from 'store/hooks'
 
 const ComponentWrapper = ({ Component, pageProps }: CustomAppProps) => {
   const role = useAppSelector(selectRole)
-  console.log({ role, allowedRoles: Component.allowedRoles })
   if (Component.allowedRoles?.length && !Component.allowedRoles.includes(role)) {
     return <h1>Unauthorized</h1>
   }
