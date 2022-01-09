db875307faeb037cb1f635996a5fe28a7622a220
Jefferson Yuri Cunha Burk <jefferson.burk@ambevtech.com.br>
2022-01-09 13:38:46 -0300

    Webpack

--- next.config.js
+++ next.config.js
@@ -1,17 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+const withAntdLess = require('next-plugin-antd-less')
+
+const { theme } = require('./src/styles/theme.js')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  swcMinify: true,
-  experimental: {
-    styledComponents: true
-  },
   reactStrictMode: true,
   ...withPWA({
     pwa: {
       dest: 'public',
       disable: !isProd
+    }
+  }),
+  ...withAntdLess({
+    modifyVars: {
+      '@primary-color': theme.colors.primaryColor
+    },
+    webpack(config) {
+      return config
     }
   })
 }
